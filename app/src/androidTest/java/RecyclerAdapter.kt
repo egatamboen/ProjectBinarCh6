444d74e2b5c459df50edace89c26ab4880a9ca1c
omegatamboen <egatamboen@gmail.com>
2022-07-04 21:40:35 +0700

    new

---
+++ app/src/androidTest/java/RecyclerAdapter.kt
@@ -0,0 +1,8 @@
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.projectbinarch6.test.R
